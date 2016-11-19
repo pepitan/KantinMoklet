077bdcd634cc330ceebe12c045c06ac158899c5d
apsarianggana@gmail.com <angganaduhitaapsari09>
2016-11-19 09:40:40 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl203122130/kantin_moklet/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl203122130.kantin_moklet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/id/sch/smktelkom_mlg/project/xirpl203122130/kantin_moklet/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xirpl203122130.kantin_moklet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
